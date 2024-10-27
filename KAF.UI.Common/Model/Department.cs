26705a44241454e4e27d13e55434e0ef0be48e8e
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2024-10-27 08:34:33 +0300

    AA

---
+++ KAF.UI.Common/Model/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAF.UI.Common.Model
+{
+    public class Department : BaseModel
+    {
+        private long _departmentId;
+        private string _departmentName;
+
+        public long DepartmentId { get => _departmentId; set => SetProperty(ref _departmentId, value); }
+        public string DepartmentName { get => _departmentName; set => SetProperty(ref _departmentName, value); }
+
+
+
+    }
+}
