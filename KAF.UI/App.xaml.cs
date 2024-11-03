4a97a4cb1286d72c47d76e396c0eff4249f33810
Major Asad <m.asaduzzaman@kuwaitarmy.gov.kw>
2024-11-03 12:27:45 +0300

    AA

--- KAF.UI/App.xaml.cs
+++ KAF.UI/App.xaml.cs
@@ -8,15 +8,8 @@ namespace KAF.UI
     /// </summary>
     public partial class App : Application
     {
-        public IConfiguration Configuration { get; private set; }
-
         protected override void OnStartup(StartupEventArgs e)
         {
-            var builder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            Configuration = builder.Build();
-
             base.OnStartup(e);
             var bootstrapper = new Bootstrapper();
             bootstrapper.Run();
